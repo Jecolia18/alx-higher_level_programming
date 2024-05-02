7af4d2a884d54fe67ecf5f57dfe9e1fcfc24e454
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-01 17:47:50 -0700

    donee

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from hidden_4 import *
+
 if __name__ == "__main__":
+    import hidden_4
+
     names = dir(hidden_4)
     for name in names:
-        if name[:2] != '_':
+        if name[:2] != '__':
             print(name)
