c1bcc2bdb143234a771c8081a0e9c9a7d727d6f4
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-06 16:15:39 -0700

    the most important is not errors but how you can fix them

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        
+
         if sys.argv[2] == "+":
-            print("{} +{} = {}".format(a, b, add(a, b)))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
-            print("{} - {} ={}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
