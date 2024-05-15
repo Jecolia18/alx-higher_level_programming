8b50170cbaa31754ac2b00b0a79a9b742d3cf19b
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-15 16:09:32 -0700

    add something

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation:
+    from calculator_1 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(i, c)
+        return (c)
+    else:
+        return(sub(a, b))
