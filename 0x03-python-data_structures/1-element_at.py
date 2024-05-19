6415870f2c9093fb384be26dc4d4b99b833f5d55
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-18 20:00:44 -0700

    list and tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """ retrieves an element from a list"""
-    if idx < 0:
+    if idx == len(my_list) or idx > len(my_list):
         return None
-    elif idx > len(my_list):
+    elif idx < 0:
         return None
     else:
         return my_list[idx]
