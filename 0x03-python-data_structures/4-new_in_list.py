b65bed981735abcdec11168d161e7c149418c9ae
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-20 14:49:17 -0700

    changing

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     """ replaces an element in a list at a specific position"""
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx > len(my_list) or idx == len(my_list):
         return my_list
     else:
         new_list = my_list[:]
