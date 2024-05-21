f9e3a7a3e38e533f6f2d0b3ea352b8ad1146226f
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-21 16:12:54 -0700

    print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     column = len(matrix[0])
     for i in range(row):
         for j in range(column):
-            print(matrix[i][j], end=" ")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
