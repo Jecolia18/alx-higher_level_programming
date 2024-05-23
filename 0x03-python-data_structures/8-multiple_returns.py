fcf8e8cb5c0d20d8836017dd362a8393886ab515
Jecolia Tieleo <jecoliatieleo@gmail.com>
2024-05-23 03:03:44 -0700

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """ returns a tuple with the length and the firs character"""
-    tup = len(sentence), sentence[0]
+    if len(sentence) == 0:
+        tup = 0, "None"
+    else:
+        tup = len(sentence), sentence[0]
     return tup
