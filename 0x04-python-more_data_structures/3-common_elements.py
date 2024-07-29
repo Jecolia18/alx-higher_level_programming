4a42701f6664d9e7daf6814e60260e418fd0a191
Jecolia <jecoliatieleo@gmail.com>
2024-07-29 10:07:56 -0700

    returns a set of common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new_set = set()
+    for wrd_1 in set_1:
+        for wrd_2 in set_2:
+            if wrd_1 == wrd_2:
+                new_set.add(wrd_1)
+    return(new_set)
