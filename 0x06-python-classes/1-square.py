4e1fcfe0dba43db089c675927f669d16bde50fc6
Jecolia <jecoliatieleo@gmail.com>
2024-08-29 16:47:24 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Square class"""
+
+
+class Square:
+    """creating the square class"""
+
+    def __init__(self, size):
+        """initiate the square
+
+        Args:
+        size (int):The size
+        """
+        self.__size = size
