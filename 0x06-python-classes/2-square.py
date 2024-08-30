ea0f78ab65aa53d7bd1d545a10fde73428393627
Jecolia <jecoliatieleo@gmail.com>
2024-08-30 07:32:56 -0700

    raised an error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
         size (int): size of the square
         """
-        if type(size) != int:
+        if isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
