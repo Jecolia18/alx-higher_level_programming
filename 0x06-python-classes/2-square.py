1c1647202fbaf6d3dc100c83d9a5f4bc13bb37f9
Jecolia <jecoliatieleo@gmail.com>
2024-08-30 07:23:14 -0700

    raised an error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""same class Square"""
+
+
+class Square:
+    """defining the class square"""
+    def __init__(self, size=0):
+        """initialize a square
+
+        Args:
+        size (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
