f7393c08aa3e6cfc2fda7a858f515a24e4d645ad
Jecolia <jecoliatieleo@gmail.com>
2024-09-05 05:46:14 -0700

    about square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -13,10 +13,12 @@ class Square:
         size (int): the size
         """
         self.size = size
+
     @property
     def size(self):
         """Get/set"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,9 +26,11 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """area of the square"""
         return (self.__size * self.__size)
+
     def my_print(self):
         """for printing"""
         for i in range(0, self.__size):
