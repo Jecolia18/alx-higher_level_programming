02bb6834b4df175311f817c5a6a2448881e0e865
Jecolia18 <jecoliatieleo@gmail.com>
2024-10-08 13:41:45 +0000

    modifyfin file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b){
-	result = parseInt(a) + parseInt(b);
-	console.log(result)
+function add (a, b) {
+  const result = parseInt(a) + parseInt(b);
+  console.log(result);
 }
-add(process.argv[2], process.argv[3])
+add(process.argv[2], process.argv[3]);
